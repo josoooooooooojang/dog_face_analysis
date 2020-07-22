4b539f83247accbd638139ae02609a9699584873
josoooooooooojang <whtnwkd@naver.com>
2020-07-22 17:09:06 +0900

    convert from 'crawling.py' to class

--- crawling.py
+++ crawling.py
@@ -1,58 +1,75 @@
+import urllib.request
+import requests
+import dlib, cv2, os
+import numpy as np
+import matplotlib.pyplot as plt
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import urllib.request
 from bs4 import BeautifulSoup
-import requests
-import os
+from imutils import face_utils
 
 def CreateFolder(directory):
     try :
         if not(os.path.isdir(directory)):
             os.makedirs(directory)  
-            # os.makedirs(os.path.join(directory))            
     except OSError:
         print('Error : Creating directory. ' + directory )
 
-# Set Keyword to save
-keywords = ['개','강아지','dog']
+class Parser():
+    def __init__(self, keywords):
+        self.keywords = keywords
+    
+    def Set(self):
+        self.driver = webdriver.Chrome("./chromedriver.exe")
+        self.root_path = './images'
 
-# Create root directory
-rootPath = './images'
-CreateFolder(rootPath)
+    def CreateDirectory(self):
+        CreateFolder(self.root_path)
+        for k in self.keywords:
+            sub_path = self.root_path + '/' + k
+            CreateFolder(sub_path)
 
-# Set 'driver.exe' (need to absoulte path)
-driver = webdriver.Chrome("C:\\Users\\조수장\\Desktop\\project\\look_like_dog\\chromedriver.exe")
+    def Crawl(self):
+        for k in self.keywords:
+            sub_path = self.root_path + '/' + k
+            base_url = 'https://pixabay.com/ko/images/search/' + k
+            self.driver.get(base_url)
+            html_source = self.driver.page_source
+            soup = BeautifulSoup(html_source)
+            page = soup.find('form',{'action':'.','method':'get'}).get_text().replace('/ ','')
+            max_page = int(page)
 
-for k in keywords:
-    subPath = rootPath+'/'+k
-    CreateFolder(subPath)
-    driver.get('https://pixabay.com/ko/images/search/'+k)
-    html = driver.page_source
-    soup = BeautifulSoup(html)
-    pagi = soup.find('form',{'action':'.','method':'get'}).get_text().replace('/ ','')
-    allPage = int(pagi)
+            n = 1
+            for p in range(1, max_page+1):
+                base_url ='https://pixabay.com/ko/images/search/' + k + '/?pagi=' + str(p)
+                self.driver.get(base_url)
+                html_source = self.driver.page_source
+                soup = BeautifulSoup(html_source)
 
-    n = 1
-    for p in range(1,allPage+1):
-        url ='https://pixabay.com/ko/images/search/'+k+'/?pagi='+str(p)
-        driver.get(url)
-        html = driver.page_source
-        soup = BeautifulSoup(html)
+                all_meta = soup.find_all('meta',{'itemprop':'contentUrl'})
+                for m in all_meta:
+                    url = m.get('content')
+                    
+                    file_name = str(n) + '.jpg'
+                    file_path = sub_path + '/' + file_name
+                    with open(file_path, 'wb') as f:
+                        response = requests.get(url, stream=True)                
 
-        objects = soup.find_all('meta',{'itemprop':'contentUrl'})
-        for o in objects:
-            link = o.get('content')
-            with open(subPath+'/'+str(n)+'.jpg', 'wb') as handle:
-                response = requests.get(link,stream=True)
+                        if not response.ok:
+                            print(response)
 
-                if not response.ok:
-                    print(response)
+                        for block in response.iter_content(1024):
+                            if not block:
+                                break
+                            f.write(block)
+                    n += 1 
+        self.driver.close
 
-                for block in response.iter_content(1024):
-                    if not block:
-                        break
 
-                    handle.write(block)   
-            n += 1
-
-driver.close
+if __name__=='__main__':
+    keywords = ['개','강아지','dog']
+    p = Parser(keywords)
+    p.Set()
+    p.CreateDirectory()
+    p.Crawl()
